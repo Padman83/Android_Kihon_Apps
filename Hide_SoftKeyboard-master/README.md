28c1721bbc45dcbce347a5f05969d582a9076780
Padman Selvamanickam <endlessgalaxy15@gmail.com>
2020-09-11 02:21:26 +0800

    Hide_SoftKeyboard

---
+++ Hide_SoftKeyboard-master/README.md
@@ -0,0 +1,21 @@
+## 📱 Hide_SoftKeyboard
+
+![ezgif com-optimize](https://user-images.githubusercontent.com/45048950/92508141-aea07e00-f23a-11ea-918a-61261d88fe83.gif)
+
+### 🧰 System Requirements
+
+* Android Studio
+* 100 % Kotlin
+
+### 🛠️ Installation 
+
+#### Open in Android Studio, Get from Version Control -> enter repository URL.
+
+```
+https://github.com/Padman83/Hide_SoftKeyboard.git
+```
+
+#### Or Use the Terminal.
+
+```
+git clone https://github.com/Padman83/Hide_SoftKeyboard.git
---
+++ Hide_SoftKeyboard-master/app/src/main/java/com/example/hide_softkeyboard/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.hide_softkeyboard
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        updateText()
+    }
+    private fun updateText(){
+        btn_button.setOnClickListener {
+            tv_text.text = et_editText.text.toString()
+
+            closeKeyboard(et_editText)
+        }
+    }
+
+    private fun closeKeyboard(view: View){
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
